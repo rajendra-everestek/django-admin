9db0db7f1f8c3329591bc290d9efefb5233da82b
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-23 16:36:27 +0530

    J-1.1

--- envsetup.sh
+++ envsetup.sh
@@ -7,12 +7,6 @@ else
     sudo apt install python3-venv -y
 fi
 
-python3 -m venv env
-source env/bin/activate
-
-
-pip3 install -r requirements.txt
-
 if [ -d "logs" ] 
 then
     echo "Log folder exists." 
--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+python3 -m venv env
 source env/bin/activate
+
+pip3 install -r requirements.txt
 
 cd /var/lib/jenkins/workspace/Django-Admin
 
