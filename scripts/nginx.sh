0d698c51c7a03d2bec7288c7316a6322585acb66
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-24 15:34:03 +0530

    Nginx

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/djangoadmin/nginx/nginx.conf /etc/nginx/sites-available/djangoadmin
+sudo cp /var/lib/jenkins/workspace/Django-Admin/nginx/nginx.conf /etc/nginx/sites-available/djangoadmin
 sudo ln -s /etc/nginx/sites-available/djangoadmin /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/djangoadmin /etc/nginx/sites-enabled
 #sudo nginx -t
