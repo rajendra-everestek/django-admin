5652872c2be7e5384b8b4dea4364a58f99c950ef
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-25 14:53:47 +0530

    q

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/sites-available/Django-Admin
 chmod 710 /var/lib/jenkins/workspace/Django-Admin
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/Django-Admin /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
